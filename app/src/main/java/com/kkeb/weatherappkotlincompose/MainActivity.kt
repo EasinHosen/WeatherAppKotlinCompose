a1726dec998f18a248fcccd46b620c6869321aa1
Easin Md. Hosen <easin.hosen@bengalmobileqa.com>
2024-11-18 22:27:07 +0600

    feat: background set

--- app/src/main/java/com/kkeb/weatherappkotlincompose/MainActivity.kt
+++ app/src/main/java/com/kkeb/weatherappkotlincompose/MainActivity.kt
@@ -4,13 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.kkeb.weatherappkotlincompose.screens.WeatherHomeScreen
 import com.kkeb.weatherappkotlincompose.ui.theme.WeatherAppKotlinComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +14,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            WeatherAppKotlinComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            WeatherApp()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
+fun WeatherApp(modifier: Modifier = Modifier) {
     WeatherAppKotlinComposeTheme {
-        Greeting("Android")
+        WeatherHomeScreen(modifier = modifier)
     }
 }
--- app/src/main/java/com/kkeb/weatherappkotlincompose/screens/WeatherHomeScreen.kt
+++ app/src/main/java/com/kkeb/weatherappkotlincompose/screens/WeatherHomeScreen.kt
@@ -1,2 +1,55 @@
 package com.kkeb.weatherappkotlincompose.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.kkeb.weatherappkotlincompose.customuis.AppBackground
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WeatherHomeScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier.fillMaxSize()
+    ) {
+        AppBackground(photoId = com.kkeb.weatherappkotlincompose.R.drawable.beautiful_skyscape_daytime_comp)
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text("Weather App", style = MaterialTheme.typography.titleLarge) },
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = Color.Transparent,
+                        actionIconContentColor = Color.White,
+                    )
+                )
+            },
+            containerColor = Color.Transparent
+        ) { padding ->
+            Surface(
+                color = Color.Transparent,
+                modifier = modifier
+                    .padding(padding)
+                    .fillMaxSize()
+                    .wrapContentSize(),
+            ) { }
+
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun WeatherHomeScreenPreview() {
+    WeatherHomeScreen()
+}
