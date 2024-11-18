a7323263b45367f5dce7e3c776fa2c8f7b7d10b7
Easin Md. Hosen <easin.hosen@bengalmobileqa.com>
2024-11-18 23:07:23 +0600

    feat: setup in-app google font(rajdhani)

--- app/src/main/java/com/kkeb/weatherappkotlincompose/utils/GoogleFontProvider.kt
+++ app/src/main/java/com/kkeb/weatherappkotlincompose/utils/GoogleFontProvider.kt
@@ -1,2 +1,18 @@
 package com.kkeb.weatherappkotlincompose.utils
 
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
+import com.kkeb.weatherappkotlincompose.R
+
+val provider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
+
+val font = GoogleFont("Rajdhani")
+
+val appFontFamily = FontFamily(
+    Font(googleFont = font, fontProvider = provider)
+)
