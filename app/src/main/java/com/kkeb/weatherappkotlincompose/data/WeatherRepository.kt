67daacfc8d45317b3daf23da4390e498a6c7b753
Easin Md. Hosen <easin.hosen@bengalmobileqa.com>
2024-11-20 10:54:03 +0600

    feat: retrofit initialized

--- app/src/main/java/com/kkeb/weatherappkotlincompose/data/WeatherRepository.kt
+++ app/src/main/java/com/kkeb/weatherappkotlincompose/data/WeatherRepository.kt
@@ -1,1 +1,35 @@
 package com.kkeb.weatherappkotlincompose.data
+
+import com.kkeb.weatherappkotlincompose.utils.WEATHER_API_Key
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+private const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
+
+//private val retrofit: Retrofit by lazy {
+//    Retrofit.Builder()
+//        .baseUrl(BASE_URL)
+//        .addConverterFactory(GsonConverterFactory.create())
+//        .build()
+//}
+
+private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+interface WeatherAPIService{
+
+    @GET("weather?lat=23.85108534653475&lon=90.4115714057402&appid=${WEATHER_API_Key}&units=metric")
+    suspend fun getCurrentWeather() : CurrentWeather
+
+    @GET("forecast?lat=23.85108534653475&lon=90.4115714057402&appid=${WEATHER_API_Key}&units=metric")
+    suspend fun getWeatherForecast() : ForecastWeather
+}
+
+object WeatherAPI{
+    val retrofitService : WeatherAPIService by lazy {
+        retrofit.create(WeatherAPIService::class.java)
+    }
+}
